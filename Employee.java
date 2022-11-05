d69476c58db397f6c909c4afff687573077ca05f
Ritutinker <110534729+Ritutinker@users.noreply.github.com>
2022-11-05 16:02:22 +0530

    OnetoOnemapping
    onetoonemapping

--- Employee.java
+++ Employee.java
@@ -9,104 +9,77 @@
  */
 package com.hibernate;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
-
+@Entity
+@Table(name="Employee_Hibernate2")
+//Entity class Employee
 public class Employee {
-	//id annotation
+	//data members
 	@Id
-	//generated auto increment value
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	//primary key join column
-	@PrimaryKeyJoinColumn(name="emp_id")
-	
-	private int emp_id;
+	@GeneratedValue
+	@Column(nullable = false)
+	public int emp_id;
+	@Column(name = "name", length = 252, nullable = true)
 	private String name;
-	private String dept;
+	@Column(name = "department", length = 128, nullable = true)
+	private String department;
 	private long salary;
-	//one to one annotation
-	@OneToOne(cascade = CascadeType.ALL)
-	private EMP_DETAILS ed_id;
-	
+
+	//no-arg constructor
 	public Employee() {
-		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public Employee(int emp_id, String name, String dept, long salary, EMP_DETAILS ed_id) {
+	//fields constructor
+	public Employee(int emp_id, String name, String department, long salary) {
 		super();
 		this.emp_id = emp_id;
 		this.name = name;
-		this.dept = dept;
+		this.department = department;
 		this.salary = salary;
-		this.ed_id = ed_id;
-		
 	}
-
-
+	//getter and setter methods
 	public int getEmp_id() {
 		return emp_id;
 	}
-
 
 	public void setEmp_id(int emp_id) {
 		this.emp_id = emp_id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-	public String getDept() {
-		return dept;
+	public String getDepartment() {
+		return department;
 	}
 
-
-	public void setDept(String dept) {
-		this.dept = dept;
+	public void setDepartment(String department) {
+		this.department = department;
 	}
-
 
 	public long getSalary() {
 		return salary;
 	}
 
-
 	public void setSalary(long salary) {
 		this.salary = salary;
 	}
 
-
-	public EMP_DETAILS getEd_id() {
-		return ed_id;
-	}
-
-
-	public void setEd_id(EMP_DETAILS ed_id) {
-		this.ed_id = ed_id;
-	}
-
+	//toString method
 	@Override
 	public String toString() {
-		return "Employee [emp_id=" + emp_id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + ", ed_id="
-				+ ed_id + "]";
+		return "Employee [emp_id=" + emp_id + ", name=" + name + ", department=" + department + ", salary=" + salary
+				+ "]";
 	}
 
-
-	
-	
-	}
+}
