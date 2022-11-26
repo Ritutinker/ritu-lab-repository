d9bac93507b73f0b338b95796c68347770891fe8
Ritutinker <110534729+Ritutinker@users.noreply.github.com>
2022-11-26 06:56:50 +0530

    product_management
    project

---
+++ SpringBoot_Hibernate/src/main/java/com/LabTest/repositories/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.LabTest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.LabTest.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	
+	public Product findById(int id);
+	@Transactional
+	int removeByPname(String pname);
+	@Transactional
+	Product findByPrice(Double price);
+	@Transactional
+	public Product findByPname(String pname);
+}
